2d3d3bcc401374a04d8a7580a5caa6beada54485
jebuk97 <jebuk97@gmail.com>
2023-10-16 23:45:48 +0900

    Update 단어 공부.py

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -10,7 +10,7 @@ for i in range(0, len(s)):
     dic[s[i]] += 1
 
 sorted_dic = sorted(dic, key = lambda x:-dic.get(x))
-#sort(dic) -> 키 값들만 빠져서 정렬을 시도. get 함수는 값으로 키를 가져옴
+#sort(dic) -> key들만 빠져서 정렬을 시도. get 함수는 키로 값 가져옴
 if len(dic) == 1:
     print(sorted_dic[0].upper())
 elif dic.get(sorted_dic[0]) == dic.get(sorted_dic[1]):
