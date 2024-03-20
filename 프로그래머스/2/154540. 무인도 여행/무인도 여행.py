f0c04b7cc8ff5a35c3f85bee26472a32722bc8a1
jebuk97 <jebuk97@gmail.com>
2024-03-20 23:34:05 +0900

    Update 무인도 여행.py

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,18 +1,17 @@
 from collections import deque
 def bfs(grid, stt, visited):
     dyx = ((1, 0), (-1, 0), (0, 1), (0, -1))
-    q = deque([(stt, int(grid[stt[0]][stt[1]]))])
+    q = deque([stt])
     visited[stt[0]][stt[1]] = True
     s = 0
     while len(q) > 0:
-        cur, score = q.popleft()
+        cur = q.popleft()
         s += int(grid[cur[0]][cur[1]])
         for d in dyx:
             # print(cur, d)
             ny, nx = (cur[0] + d[0], cur[1] + d[1])
             if 0 <= ny < len(grid) and 0 <= nx < len(grid[0]) and grid[ny][nx] != 'X' and visited[ny][nx] is False:
-                score += int(grid[ny][nx])
-                q.append(((ny, nx), score))
+                q.append((ny, nx))
                 visited[ny][nx] = True
             
     return s, grid, visited
